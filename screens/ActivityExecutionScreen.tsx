74da89b0760bc3694a47f5d2726870b40bdca053
Lane Garner <b.lane.garner@gmail.com>
2026-06-18 22:19:04 -0500

    fix(sets): enable execution field editing

--- screens/ActivityExecutionScreen.tsx
+++ screens/ActivityExecutionScreen.tsx
@@ -506,7 +506,6 @@ export default function ActivityExecutionScreen({ navigation, route }: any) {
                   }}
                   inputRefs={setInputRefs}
                   onInputFocus={scrollToSetInput}
-                  readOnly
                 />
               ))}
 
